7ee64cd59dddc956b4217a1f66bcb0d94c6a4f32
Richard Blythman <richardblythman@gmail.com>
2024-11-15 15:49:30 +0000

    improve schema abstractions and config loading

--- simple_chat_agent/run.py
+++ simple_chat_agent/run.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 from dotenv import load_dotenv
+from litellm import completion
+from naptha_sdk.schemas import AgentDeployment
 import os
 from simple_chat_agent.schemas import InputSchema
 from simple_chat_agent.utils import get_logger
-from litellm import completion
-import yaml
 
 load_dotenv()
 
@@ -12,22 +12,22 @@ logger = get_logger(__name__)
 
 class SimpleChatAgent:
 
-    def __init__(self, cfg: dict):
-        self.cfg = cfg
+    def __init__(self, agent_deployment: AgentDeployment):
+        self.agent_deployment = agent_deployment
 
     def chat(self, inputs: InputSchema):
 
         messages = [msg for msg in inputs.tool_input_data if msg["role"] != "system"]
-        messages.insert(0, {"role": "system", "content": self.cfg["inputs"]["system_message"]})
+        messages.insert(0, {"role": "system", "content": self.agent_deployment.agent_config.system_prompt.model_dump_json()})
 
-        api_key = None if inputs.llm_backend == "ollama" else ("EMPTY" if inputs.llm_backend == "vllm" else os.getenv("OPENAI_API_KEY"))
+        api_key = None if self.agent_deployment.agent_config.llm_config.client == "ollama" else ("EMPTY" if self.agent_deployment.agent_config.llm_config.client == "vllm" else os.getenv("OPENAI_API_KEY"))
 
         response = completion(
-            model=self.cfg["models"][inputs.llm_backend]["model"],
+            model=self.agent_deployment.agent_config.llm_config.model,
             messages=messages,
-            temperature=self.cfg["models"][inputs.llm_backend]["temperature"],
-            max_tokens=self.cfg["models"][inputs.llm_backend]["max_tokens"],
-            api_base=self.cfg["models"][inputs.llm_backend]["api_base"],
+            temperature=self.agent_deployment.agent_config.llm_config.temperature,
+            max_tokens=self.agent_deployment.agent_config.llm_config.max_tokens,
+            api_base=self.agent_deployment.agent_config.llm_config.api_base,
             api_key=api_key
         )
 
@@ -38,34 +38,38 @@ class SimpleChatAgent:
 
         return messages
 
-def run(inputs: InputSchema, *args, **kwargs):
+def run(inputs: InputSchema, agent_deployment: AgentDeployment, *args, **kwargs):
     logger.info(f"Running with inputs {inputs.tool_input_data}")
-    cfg = kwargs["cfg"]
-    logger.info(f"cfg: {cfg}")
 
-    simple_chat_agent = SimpleChatAgent(cfg)
+    simple_chat_agent = SimpleChatAgent(agent_deployment)
 
     method = getattr(simple_chat_agent, inputs.tool_name, None)
 
     return method(inputs)
 
 
-
 if __name__ == "__main__":
+    from naptha_sdk.client.naptha import Naptha
+    from naptha_sdk.schemas import AgentRunInput
+    from naptha_sdk.configs import load_agent_deployments
 
-    cfg_path = f"simple_chat_agent/component.yaml"
-    with open(cfg_path, "r") as file:
-        cfg = yaml.load(file, Loader=yaml.FullLoader)
+    naptha = Naptha()
 
-    messages = [
-        {"role": "user", "content": "tell me a joke"},
-    ]
+    # Configs
+    agent_deployments = load_agent_deployments("simple_chat_agent/configs/agent_deployments.json")
 
-    inputs = InputSchema(
+
+    input_params = InputSchema(
         tool_name="chat",
-        tool_input_data=messages,
-        llm_backend="openai"
+        tool_input_data=[{"role": "user", "content": "tell me a joke"}],
     )
-    response = run(inputs, cfg=cfg)
+
+    agent_run = AgentRunInput(
+        inputs=input_params,
+        agent_deployment=agent_deployments[0],
+        consumer_id=naptha.user.id,
+    )
+
+    response = run(input_params, agent_deployments[0], naptha.user.id)
 
 
--- simple_chat_agent/schemas.py
+++ simple_chat_agent/schemas.py
@@ -3,4 +3,3 @@ from pydantic import BaseModel
 class InputSchema(BaseModel):
     tool_name: str
     tool_input_data: list
-    llm_backend: str = "ollama"
